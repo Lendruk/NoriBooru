ca46b6847e433537f93e2f96e50be7ef9a96be39
Lendruk <fenixeye3@gmail.com>
2024-03-11 20:00:33 +0000

    Add keyboard shortcuts to playlist view and have images take fullscreen

--- frontend/src/routes/playlists/view/[id]/+page.svelte
+++ frontend/src/routes/playlists/view/[id]/+page.svelte
@@ -5,6 +5,9 @@
 	import { HttpService } from "$lib/services/HttpService";
 	import type { MediaItem } from "$lib/types/MediaItem";
 	import type { Playlist } from "$lib/types/Playlist";
+	import ArrowRight from "$lib/icons/ArrowRight.svelte";
+	import ArrowLeft from "$lib/icons/ArrowLeft.svelte";
+	import { goto } from "$app/navigation";
 
   let currentMediaIndex = $state(0);
   let isPaused = $state(false);
@@ -54,12 +57,28 @@
       currentMediaIndex = currentMediaIndex + 1;
     }
   }
+
+  function onKeyDown(e: KeyboardEvent) {
+		 switch(e.key) {
+        case "ArrowLeft":
+          handleClickLeft();
+          break;
+        case "ArrowRight":
+          handleClickRight();
+          break;
+        case "Escape": 
+          goto(`/playlists`);
+          break;
+		 }
+	}
 </script>
 
-<div class="flex flex-row flex-1 h-full">
-  <button on:click={handleClickLeft} class={`flex justify-center items-center w-1/12 bg-slate-900 ${currentMediaIndex - 1 < 0 && 'cursor-not-allowed'}`}>&lt;</button>
+<div class="flex flex-row flex-1 h-full absolute top-0 left-0 w-full">
+  <button on:click={handleClickLeft} class={`flex fill-white justify-center items-center w-1/12 bg-red-950 hover:bg-red-900 hover:transition ${currentMediaIndex - 1 < 0 && 'cursor-not-allowed'}`}>
+    <ArrowLeft />
+  </button>
   {#if currentMediaItem}
-    <div class="flex flex-1 flex-col justify-center items-center">
+    <div class="flex flex-1 flex-col justify-center items-center bg-zinc-800 backdrop-blur-lg bg-opacity-5">
       {#if currentMediaItem.type === "image"}
         <img class="bg-cover" src={`${HttpService.BASE_URL}/images/${HttpService.getVaultId()}/${currentMediaItem.fileName}.${currentMediaItem.extension}`} alt="gallery-img" />
       {/if}
@@ -69,7 +88,7 @@
         </video>
       {/if}
 
-      <button on:click={handlePause} class={`absolute bottom-0 ${timePerItem ? 'flex' : 'hidden'}`}>
+      <button on:click={handlePause} class={`absolute flex items-center fill-white bg-red-950 rounded-full p-4 hover:bg-red-900 hover:transition bottom-0 ${timePerItem ? 'flex' : 'hidden'}`}>
         {#if isPaused}
           <PlayIcon width={50} height={50} />
         {:else}
@@ -78,8 +97,12 @@
       </button>
     </div>
   {/if}
-  <button on:click={handleClickRight} class={`flex justify-center items-center w-1/12 bg-slate-900 ${currentMediaIndex + 1 >= items.length && 'cursor-not-allowed'}`}>&gt;</button>
+  <button on:click={handleClickRight} class={`flex justify-center bg-red-950 hover:bg-red-900 hover:transition fill-white items-center w-1/12 ${currentMediaIndex + 1 >= items.length && 'cursor-not-allowed'}`}>
+    <ArrowRight />
+  </button>
 </div>
+
+<svelte:window on:keydown={onKeyDown} />
 
 <svelte:head>
   <title>NoriBooru - Playlist View</title>
