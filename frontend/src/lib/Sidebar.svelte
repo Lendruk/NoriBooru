bffa74f34d5933fd608d60e9f88352de3b529580
Lendruk <fenixeye3@gmail.com>
2024-03-18 22:50:05 +0000

    Add collapse support to sidebar

--- frontend/src/lib/Sidebar.svelte
+++ frontend/src/lib/Sidebar.svelte
@@ -6,6 +6,10 @@
   import { PUBLIC_VERSION } from '$env/static/public';
 	import InboxIcon from './icons/InboxIcon.svelte';
 	import UploadIcon from './icons/UploadIcon.svelte';
+	import ArrowRight from './icons/ArrowRight.svelte';
+	import ArrowLeft from './icons/ArrowLeft.svelte';
+
+	let isSidebarOpen = true;
 
 	type Route = {
 		name: string;
@@ -65,26 +69,42 @@
 	}
 </script>
 
-<aside class="bg-zinc-900 flex flex-col justify-between rounded-tr-md rounded-br-md w-2/12 max-w-[200px]">
+<aside class={`bg-zinc-900 flex flex-col justify-between rounded-tr-md rounded-br-md max-w-[200px] relative ${isSidebarOpen ? 'w-2/12' : 'w-[50px]'}`}>
   <div>
     {#each routes as route}
       <a
         class={`${
           isCurrentPathSelected(route) && 'bg-red-950 text-white'
         } pl-4 pr-4 pt-2 pb-2 text-md flex items-center gap-4 hover:bg-red-950 hover:bg-slate-300 hover:text-white hover:text-zinc-800 hover:transition-all`}
-        href={route.navHref}>{#if route.icon} <svelte:component this={route.icon} width={16} class="fill-white" height={16} color="#FFFFFF"/> {/if}<div>{route.name}</div></a
-      >
+        href={route.navHref}>
+				{#if route.icon} 
+					<svelte:component this={route.icon} width={16} class="fill-white" height={16} color="#FFFFFF"/> 	
+				{/if}
+				{#if isSidebarOpen}
+					<div>{route.name}</div>
+				{/if}
+			</a>
     {/each}
   </div>
-  <div class="flex flex-col text-xs items-center mb-4">
-    <div>NoriBooru</div>
-    <div>v{PUBLIC_VERSION}</div>
-  </div>
+	{#if isSidebarOpen}
+		<div class="flex flex-col text-xs items-center mb-4">
+			<div>NoriBooru</div>
+			<div>v{PUBLIC_VERSION}</div>
+		</div>
+	{/if}
+	<button on:click={() => isSidebarOpen = !isSidebarOpen} class={`${isSidebarOpen ? 'absolute right-0 bottom-0 rounded-tl-lg' : 'w-full flex items-center justify-center'}  bg-red-950 p-2 fill-white hover:bg-red-900 hover:transition`}>
+		{#if isSidebarOpen}
+			<ArrowLeft />
+		{:else}
+			<ArrowRight />
+		{/if}
+	</button>
 </aside>
 
 <style>
 	aside {
 		color: #fff;
+		transition: width 0.2s ease-in-out;
 	}
 
 	a {
