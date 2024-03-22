c3f0dccfab158af25b8c7c7f6304223d39c0bfd3
Lendruk <fenixeye3@gmail.com>
2024-03-22 13:07:02 +0000

    Fix playlist creation not saving timePerItem

--- backend/src/routes/playlists/createPlaylist.ts
+++ backend/src/routes/playlists/createPlaylist.ts
@@ -6,30 +6,31 @@ import { checkVault } from '../../hooks/checkVault';
 type RequestBody = {
   name: string,
   randomizeOrder: boolean,
+  timePerItem: number,
   items: number[]
 };
 
 const createPlaylist = async (request: Request, reply: FastifyReply) => {
-  const vault = request.vault;
+	const vault = request.vault;
 
-  if(!vault) {
-    return reply.status(400).send('No vault provided');
-  }
+	if(!vault) {
+		return reply.status(400).send('No vault provided');
+	}
 
-  const { db } = vault;
-  const body = request.body as RequestBody;
-  const playlist = (await db.insert(playlists).values({ name: body.name, createdAt: Date.now(), randomizeOrder: body.randomizeOrder ? 1 : 0 }).returning())[0];
+	const { db } = vault;
+	const body = request.body as RequestBody;
+	const playlist = (await db.insert(playlists).values({ name: body.name, createdAt: Date.now(), randomizeOrder: body.randomizeOrder ? 1 : 0, timePerItem: body.timePerItem }).returning())[0];
 
-  let items: { playlistId: number; mediaItemId: number; itemIndex: number }[] = [];
-  if (body.items.length > 0) {
-    items = await db.insert(playlists_mediaItems_table).values(body.items.map((item, index) => ({ playlistId: playlist.id, mediaItemId: item, itemIndex: index }))).returning();
-  }
-  return reply.send({...playlist, items });
+	let items: { playlistId: number; mediaItemId: number; itemIndex: number }[] = [];
+	if (body.items.length > 0) {
+		items = await db.insert(playlists_mediaItems_table).values(body.items.map((item, index) => ({ playlistId: playlist.id, mediaItemId: item, itemIndex: index }))).returning();
+	}
+	return reply.send({...playlist, items });
 };
 
 export default {
 	method: 'POST',
 	url: '/playlists',
 	handler: createPlaylist,
-  onRequest: checkVault,
+	onRequest: checkVault,
 } as RouteOptions;
