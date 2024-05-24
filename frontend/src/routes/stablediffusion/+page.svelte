549fa963f44b33efacc14a38fafc14d518acd309
Lendruk <fenixeye3@gmail.com>
2024-05-24 18:33:59 +0100

    WIP img gen ui

--- frontend/src/routes/stablediffusion/+page.svelte
+++ frontend/src/routes/stablediffusion/+page.svelte
@@ -1,3 +1,12 @@
-<div>
-  AI 
+<div class="m-2 bg-zinc-900 rounded-md p-4 flex flex-col">
+  <div class="flex flex-col">
+    <div class="flex flex-col">
+      <div>Prompt</div>
+      <textarea />
+    </div>
+    <div class="flex flex-col">
+      <div>Negative Prompt</div>
+      <textarea />
+    </div>
+  </div>
 </div>
