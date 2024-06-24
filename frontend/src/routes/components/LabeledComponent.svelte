00e0e0b20e1e1e4cfcf2195a202bf1fae400bb58
Lendruk <fenixeye3@gmail.com>
2024-06-24 19:22:53 +0100

    Improve prompt search styles & add more information

---
+++ frontend/src/routes/components/LabeledComponent.svelte
@@ -0,0 +1,7 @@
+
+<div class="flex flex-col mt-2 mb-2">
+  <div class="text-lg mb-1">
+    <slot name="label" />
+  </div>
+  <slot name="content"/>
+</div>
--- frontend/src/routes/stablediffusion/generator/components/PromptSearch.svelte
+++ frontend/src/routes/stablediffusion/generator/components/PromptSearch.svelte
@@ -4,20 +4,33 @@
 	import TrashIcon from "$lib/icons/TrashIcon.svelte";
 	import { HttpService } from "$lib/services/HttpService";
 	import type { SavedPrompt } from "$lib/types/SavedPrompt";
-	import { onMount } from "svelte";
+	import LabeledComponent from "../../../components/LabeledComponent.svelte";
 
   export let isOpen: boolean;
   export let onSelectPrompt: (prompt: SavedPrompt) => void | Promise<void>;
 
   let prompts: SavedPrompt[] = [];
   let currentOpenPrompt: SavedPrompt | undefined;
+  let currentOpenPromptName: string | undefined;
 
   async function getSavedPrompts() {
     const response = await HttpService.get<SavedPrompt[]>('/sd/prompts');
     prompts = response;
 
     currentOpenPrompt = prompts[0];
+    if (currentOpenPrompt) {
+      currentOpenPromptName = currentOpenPrompt.name;
+    }
   }  
+
+  async function updatePrompt() {
+    if (!currentOpenPrompt) return;
+
+    await HttpService.put(`/sd/prompts/${currentOpenPrompt.id}`, { ...currentOpenPrompt, name: currentOpenPromptName });
+    const index = prompts.findIndex(prompt => prompt.id === currentOpenPrompt!.id);
+    prompts[index].name = currentOpenPromptName ?? 'Unnamed';
+    prompts = prompts;
+  }
 
   async function deleteSavedPrompt(id: string) {
     await HttpService.delete(`/sd/prompts/${id}`);
@@ -47,40 +60,89 @@
       isOpen = false;
     }
   }
+
+  function onBackgroundClick() {
+    isOpen = false;
+}
 </script>
 
-<div class="absolute flex items-center justify-center h-full w-full top-0 left-0 z-50 backdrop-blur-sm">
-  <div class="bg-zinc-900 min-h-[80%] min-w-[80%] p-2">
-    <div class="text-4xl">
+<!-- svelte-ignore a11y_click_events_have_key_events -->
+<!-- svelte-ignore a11y_no_static_element_interactions -->
+<div on:click={onBackgroundClick} class="absolute flex items-center justify-center h-full w-full top-0 left-0 z-50 backdrop-blur-sm">
+  <div on:click={(e) => e.stopPropagation()} class="bg-zinc-900 min-h-[80%] min-w-[80%] p-2">
+    <div class="text-4xl pb-2">
       Search saved prompts
     </div>
     <div class="flex">
-      <div class="flex-[0.2]">
+      <div class="flex-[0.25]">
         {#each prompts as prompt}
-          <div class="flex">
-            <Button class="rounded-none h-[40px]" onClick={() => currentOpenPrompt = prompt }>{prompt.name ?? 'unnamed'}</Button>
+          <div class="flex justify-between">
+            <Button class="rounded-none h-[40px] flex flex-1" onClick={() => {currentOpenPrompt = prompt; currentOpenPromptName = prompt.name} }>{prompt.name ?? 'unnamed'}</Button>
             <Button class="rounded-none h-[40px]" onClick={() => deleteSavedPrompt(prompt.id!)}><TrashIcon /></Button>
           </div>
         {/each}
       </div>
-      <div class="flex flex-1 flex-col">
+      <div class="flex flex-1 flex-col pl-2">
         {#if currentOpenPrompt}
           <div>
             <div>
-              <div>
-                Positive Prompt
+              <LabeledComponent>
+                <div slot="label">
+                  Prompt name
+                </div>
+                <div slot="content" class="flex">
+                  <input type="text" bind:value={currentOpenPromptName}  /> <Button onClick={() => updatePrompt()}>Update</Button>
+                </div>
+              </LabeledComponent>
+              <LabeledComponent>
+                <div slot="label">
+                  Positive Prompt
+                </div>
+                <div slot="content">
+                  {currentOpenPrompt.positivePrompt || 'No positive prompt'}
+                </div>
+              </LabeledComponent>
+              <LabeledComponent>
+                <div slot="label">
+                  Negative Prompt
+                </div>
+                <div slot="content">
+                  {currentOpenPrompt.negativePrompt || 'No negative prompt'}
+                </div>
+              </LabeledComponent>
+              <div class="text-xl">Settings</div>
+              <div class="flex gap-4">
+                <LabeledComponent>
+                  <div slot="label">Size</div>
+                  <div slot="content">
+                    <div>width: {currentOpenPrompt.width}</div>
+                    <div>height: {currentOpenPrompt.height}</div>
+                  </div>
+                </LabeledComponent>
+                <LabeledComponent>
+                  <div slot="label">Sampling</div>
+                  <div slot="content">
+                    <div>sampler: {currentOpenPrompt.sampler}</div>
+                    <div>steps: {currentOpenPrompt.steps}</div>
+                  </div>
+                </LabeledComponent>
               </div>
               <div>
-                {currentOpenPrompt.positivePrompt || 'No positive prompt'}
-              </div>
-              <div>
-                Negative Prompt
-              </div>
-              <div>
-                {currentOpenPrompt.negativePrompt || 'No negative prompt'}
+                <LabeledComponent>
+                  <div slot="label">High res</div>
+                  <div slot="content">
+                    <div>Using high res: {!!currentOpenPrompt.highRes}</div>
+                    {#if currentOpenPrompt.highRes}
+                      <div>Upscaler: {currentOpenPrompt.highRes.upscaler}</div>
+                      <div>Upscale by: {currentOpenPrompt.highRes.upscaleBy}</div>
+                      <div>Denoising strength: {currentOpenPrompt.highRes.denoisingStrength}</div>
+                      <div>Steps: {currentOpenPrompt.highRes.steps}</div>
+                    {/if}
+                  </div>
+                </LabeledComponent>
               </div>
             </div>
-            <Button onClick={() => { onSelectPrompt(currentOpenPrompt!); isOpen = false; }}>Select</Button>
+            <Button class="h-[40px]" onClick={() => { onSelectPrompt(currentOpenPrompt!); isOpen = false; }}>Select</Button>
           </div>
         {:else}
           <div class="text-8xl">
