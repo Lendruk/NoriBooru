acbfa99064ef4988cf2646c53f53c1a9068ffcea
Lendruk <fenixeye3@gmail.com>
2024-07-16 22:33:30 +0100

    Change TextArea styling to be inline with other inputs

--- frontend/src/lib/components/TextArea.svelte
+++ frontend/src/lib/components/TextArea.svelte
@@ -2,4 +2,7 @@
 	export let value: string;
 </script>
 
-<textarea bind:value class="text-black"></textarea>
+<textarea
+	class="focus:outline-none bg-surface-color border-red-950 border-2 rounded-md shadow-xl"
+	bind:value
+></textarea>
