11e473ab5043d6d4009f125dcf4e45e44df202fd
Lendruk <fenixeye3@gmail.com>
2024-08-13 20:01:43 +0100

    Add bump version scripts

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -10,6 +10,8 @@ cp ./scripts/runProdServer.sh ./build/temp/
 cp -r ./backend/dist/ ./build/temp/
 mv ./build/temp/dist/ ./build/temp/server
 
+PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
+echo $PACKAGE_VERSION
 
-tar cvzf ./build/nori-booru-linux-x64.tar.gz -C ./build/temp .
+tar cvzf ./build/nori-booru-$PACKAGE_VERSION-x64.tar.gz -C ./build/temp .
 rm -rf ./build/temp
