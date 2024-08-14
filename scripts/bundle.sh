8a723fadc85a258d922754f071fa4983b278776e
Lendruk <fenixeye3@gmail.com>
2024-08-14 14:33:32 +0100

    Update pipeline and bundle script

--- scripts/bundle.sh
+++ scripts/bundle.sh
@@ -1,17 +1,30 @@
 
 # Remove old build
+echo "Starting app bundle"
+
+echo "Removing old build"
 rm -rf ./build
 
 mkdir ./build
 mkdir ./build/temp
 
-cp ./src-tauri/target/release/bundle/appimage/*.AppImage ./build/temp
-cp ./scripts/runProdServer.sh ./build/temp/
-cp -r ./backend/dist/ ./build/temp/
-mv ./build/temp/dist/ ./build/temp/server
-
 PACKAGE_VERSION=$(node -p -e "require('./package.json').version")
 echo $PACKAGE_VERSION
 
-tar cvzf ./build/nori-booru-$PACKAGE_VERSION-x64.tar.gz -C ./build/temp .
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  cp ./src-tauri/target/release/bundle/appimage/*.AppImage ./build/temp
+  cp ./scripts/runProdServer.sh ./build/temp/
+  cp -r ./backend/dist/ ./build/temp/
+  mv ./build/temp/dist/ ./build/temp/server
+
+  tar cvzf ./build/nori-booru-$PACKAGE_VERSION-x64.tar.gz -C ./build/temp .
+elif [[ "$OSTYPE" == "msys" ]]; then
+  cp ./src-tauri/target/release/bundle/nsis/*.exe ./build/temp
+  cp ./scripts/runProdServer.sh ./build/temp/
+  cp -r ./backend/dist/ ./build/temp/
+  mv ./build/temp/dist/ ./build/temp/server
+
+  tar cvzf ./build/nori-booru-$PACKAGE_VERSION\_x64.windows.tar.gz -C ./build/temp .
+fi
+
 rm -rf ./build/temp
